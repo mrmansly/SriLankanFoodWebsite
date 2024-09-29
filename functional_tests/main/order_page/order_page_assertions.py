ba221edb7521bfd17c25dda660b27a9578ee4ddd
Graeme Eaton <mrmansly@iinet.net.au>
2024-09-29 16:47:25 +0800

    FWP-14: Fix bug with empty rice cooker menu link

--- functional_tests/main/order_page/order_page_assertions.py
+++ functional_tests/main/order_page/order_page_assertions.py
@@ -1,3 +1,3 @@
-class OrderDetailsPageAssertions:
+class OrderPageAssertions:
     TITLE="Order Details"
     
--- functional_tests/main/testing_utils.py
+++ functional_tests/main/testing_utils.py
@@ -33,9 +33,9 @@ def get_web_driver():
 
 def get_chrome_driver():
     chrome_options = ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
     service = ChromeService()
     return webdriver.Chrome(service=service, options=chrome_options)
 
